f84287dafb42e180a9bc3da46fb6d0753203d838
jenseneducation-hala-terk <54705184+jenseneducation-hala-terk@users.noreply.github.com>
2019-12-05 04:04:12 +0100

    modified

--- app.js
+++ app.js
@@ -5,5 +5,10 @@ app.use("/static", express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-
+app.get("/signup", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+app.get("/submission", (req, res) => {
+  res.sendFile(__dirname + "/submission.html");
+});
 app.listen(8080);
